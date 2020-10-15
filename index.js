879be46436dabc30f672656b24f893ca796eaf7a
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-10-14 20:35:18 -0500

    added all questions for the prompt

--- index.js
+++ index.js
@@ -5,18 +5,56 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [{
     type: "input",
-    name: "Git-Hub",
-    message: "What is your Git-Hub username?"},
-    {
+    name: "tile",
+    message: "What is the title?"
+    }, {
     type: "input",
     name: "Description",
     message: "How would you describe your REPO?"
-    }
+    },{
+    type: "input",
+    name: "Contents",
+    message: "What are the contents?"
+    },{
+    type: "input",
+    name: "Installation",
+    message: "What is the installation?"
+    },{
+        type: "input",
+        name: "Usage",
+        message: "How much was the usage?"
+        },{
+        type: "input",
+        name: "License",
+        message: "What type of license is being used?"
+        },{
+        type: "input",
+        name: "Contributing",
+        message: "If you have contributors, list them?"
+    }, {
+        type: "input",
+        name: "Test",
+        message: "Can you display a test?"
+        },{
+        type: "input",
+        name: "Question",
+        message: "Do you have any questions to display?"
+        },{
+        type: "input",
+        name: "Git-Hub",
+        message: "What is your Git-Hub username?"
+        }
     ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeToFile(fileName, JSON.stringify(data), function(err){
+        if (err){
+            console.log("Oops! do it again!")
+        } else {
+            console.log("All Good to Go!")
+        }
+    })
 }
 
 // function to initialize program
